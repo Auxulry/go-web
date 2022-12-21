f13ab886c1b9ffee98b645dd98b9eab4348a0e32
Mochamad akbar <mochakbar912@gmail.com>
2022-12-21 17:32:56 +0700

    feat: template caching

---
+++ template_caching_test.go
@@ -0,0 +1,27 @@
+package go_web
+
+import (
+	"fmt"
+	"html/template"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var myTemplate = template.Must(template.ParseFS(templates, "templates/*.gohtml"))
+
+func TemplateCaching(w http.ResponseWriter, r *http.Request) {
+	myTemplate.ExecuteTemplate(w, "simple.gohtml", "Hello Template Caching")
+}
+
+func TestTemplateCaching(t *testing.T) {
+	request := httptest.NewRequest(http.MethodGet, "http://localhost:8080", nil)
+	recorder := httptest.NewRecorder()
+
+	TemplateCaching(recorder, request)
+
+	body, _ := io.ReadAll(recorder.Result().Body)
+
+	fmt.Println(string(body))
+}
